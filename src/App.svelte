b7710407879f12d9157233af76e0d898e94d2c84
Marcin <jerzakm@gmail.com>
2020-09-16 16:19:13 +0200

    simple nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
+  // @ts-ignore
   import Router, { push } from 'svelte-spa-router'
-  import Login from './views/Login.svelte'
   import Home from './Home.svelte'
+  import ByPart from './views/ByPart.svelte'
+  import ByProduct from './views/ByProduct.svelte'
+  import PartTable from './views/PartTable.svelte'
+  import Header from './components/Header.svelte'
   import 'smelte/src/tailwind.css'
-
-  import { userAuth } from './stores'
-  let loggedIn = false
-  userAuth.subscribe((user) => {
-    console.log(user)
-    if (user == undefined) {
-      push('/login')
-      loggedIn = false
-    } else {
-      console.log('app logged in')
-      loggedIn = true
-    }
-  })
 
   const routes = {
     '/': Home,
-    '/login': Login,
+    '/by-product': ByProduct,
+    '/by-part': ByPart,
+    '/part-table': PartTable,
   }
 </script>
 
-<main>
-  {#if loggedIn}
-    <h1>Main page</h1>
-  {/if}
+<main class="p-20 pt-10">
+  <Header />
   <Router {routes} />
 </main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,39 @@
+<script>
+  import {
+    link,
+    push,
+    pop,
+    replace,
+    location,
+    querystring,
+  } from 'svelte-spa-router'
+  import active from 'svelte-spa-router/active'
+</script>
+
+<div class="flex flex-column items-center justify-between">
+  <h1 class="flex-1">Katalog części foteli</h1>
+  <nav class="flex-1 flex flex-column items-end justify-end">
+    <a
+      href="/by-products"
+      class="text-3xl mr-10"
+      use:link
+      use:active={{ path: '/by-products', className: 'text-porange-900' }}>
+      Produkty
+    </a>
+    <a
+      href="/by-parts"
+      class="text-3xl mr-10"
+      use:link
+      use:active={{ path: '/by-parts', className: 'text-porange-900' }}>
+      Części
+    </a>
+    <a
+      href="/parts-table"
+      class="text-3xl"
+      use:link
+      use:active={{ path: '/parts-table', className: 'text-porange-900' }}>
+      Tabela
+    </a>
+  </nav>
+
+</div>
